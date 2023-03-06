7a97dc415a150caf98a2898bb22ec38aab50b322
onepopina <anyarasp@gmail.com>
2023-03-06 16:00:13 +0100

    fix reduce

--- src/index.js
+++ src/index.js
@@ -14,15 +14,21 @@ fetch(URL)
 
 const createListItem = item =>
   `<li class="list__item">
-  <img class="list__pic" src="${item.urlToImage}" alt="${item.description}">
+${
+  item.urlToImage
+    ? `<img src="${item.urlToImage}" alt="${item.description}">`
+    : ''
+}
   <h2 class="list__title">${item.title}</h2>
   <p class="list__description">${item.description ? item.description : ''}</p>
   <p class="list__author">${item.author ? item.author : ''}</p>
   <a class="list__link" href="${item.url}" target="_blank">Read more</a>
   </li>`;
 
+// const generateContent = array =>
+//   array.reduce((acc, item) => acc + createListItem(item), '');
 const generateContent = array =>
-  array.reduce((acc, item) => acc + createListItem(item), '');
+  array ? array.reduce((acc, item) => acc + createListItem(item), '') : '';
 
 const insertContent = array => {
   const result = generateContent(array);
